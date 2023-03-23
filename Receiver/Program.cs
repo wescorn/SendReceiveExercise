4ca594ba9e21df970d6c5ad8277301d41652e57f
Søren <act.wescorn@gmail.com>
2023-03-23 10:24:05 +0100

    Initial commit

---
+++ Receiver/Program.cs
@@ -0,0 +1,22 @@
+﻿using EasyNetQ;
+using Messages;
+using OpenTelemetry.Context.Propagation;
+
+public class Program
+{
+    private static readonly TextMapPropagator Propagator = new TraceContextPropagator();
+    
+    public static void Main(string[] args)
+    {
+        var bus = RabbitHutch.CreateBus("host=localhost;username=application;password=pepsi");
+        bus.PubSub.SubscribeAsync<Message>("DTE", msg =>
+        {
+            using (var activity = Monitoring.Monitoring.ActivitySource.StartActivity())
+            {
+                Console.WriteLine(msg.Text);
+            }
+        });
+        
+        while(true) { Thread.Sleep(5000); }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,16 @@
+﻿using EasyNetQ;
+using Messages;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using(var activity = Monitoring.Monitoring.ActivitySource.StartActivity())
+        {
+            var bus = RabbitHutch.CreateBus("host=localhost;username=application;password=pepsi");
+            var message = new Message("Hello World");
+            bus.PubSub.PublishAsync(message);
+        }
+        Thread.Sleep(5000); // Give Zipkin time to receive the event
+    }
+}
